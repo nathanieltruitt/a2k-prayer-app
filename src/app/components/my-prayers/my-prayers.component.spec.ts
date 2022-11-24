3fcbff13735f939c475b1d774ca5f0312a90baa8
Nathaniel Truitt <nathantruitt2@gmail.com>
2022-11-23 21:28:16 -0500

    adding pages

---
+++ src/app/components/my-prayers/my-prayers.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyPrayersComponent } from './my-prayers.component';
+
+describe('MyPrayersComponent', () => {
+  let component: MyPrayersComponent;
+  let fixture: ComponentFixture<MyPrayersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MyPrayersComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MyPrayersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/my-prayers/my-prayers.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './my-prayers.component.html',
+  styleUrls: ['./my-prayers.component.css']
+})
+export class MyPrayersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
